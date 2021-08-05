437c8ecad734bbbc853628ae4b294401eda6624c
Gustavo Azevedo <gustavosazevedo@gmail.com>
2021-08-05 15:51:42 -0300

    add estrutura modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,29 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalIsOpen, setIsNewTransactionModalIsOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalIsOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalIsOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal isOpen={isNewTransactionModalIsOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
     </>
   );
